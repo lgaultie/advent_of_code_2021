fbd36c4d61b23c790a5eb9b044d5c69f9290a81e
HeyLor <loraine@heywear.com>
2022-01-11 15:34:42 +0100

    feat(day01): completed day01 ex01

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # advent_of_code_2021
+
+To run:
+
+- Install dart
+- dart run day01/day01.dart input
---
+++ day01/day01.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+void checkArguments(List<String> arguments) {
+  if (arguments.length != 1)
+  {
+    print('No file, or too much files in input');
+    exit(0);
+  }
+}
+
+void main(List<String> arguments) {
+  checkArguments(arguments);
+  try {
+    File file = File(arguments[0]);
+
+    List<String> lines = file.readAsLinesSync();
+    List<int> numbers = [];
+    for (var element in lines) {
+      numbers.add(int.parse(element));
+    }
+    int increased = 0;
+    for (int i = 0; i < numbers.length; i++) {
+      if(i > 0 && numbers[i] > numbers[i-1]) {
+        increased++;
+      }
+    }
+    print(increased);
+  }
+  on FileSystemException {
+    print('Could not read file');
+    exit(0);
+  }
+  catch (e) {
+    print(e);
+    exit(0);
+  }
+}
