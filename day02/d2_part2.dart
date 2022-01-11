7fc10fc35d41b0901ca9b5753e61c491a2de18d2
HeyLor <loraine@heywear.com>
2022-01-11 17:44:45 +0100

    feat(day02): completed day02 part2

--- day02/d2_part2.dart
+++ day02/d2_part2.dart
@@ -10,16 +10,18 @@ void main(List<String> args) {
     var contents = file.readAsLinesSync();
     var horizontal = 0;
     var depth = 0;
+    var aim = 0;
     for (var elem in contents) {
       if (elem.contains(' ')) {
         var instruction = elem.split(' ')[0];
         var number = int.parse(elem.split(' ')[1]);
         if (instruction == 'forward') {
           horizontal += number;
+          depth += aim * number;
         } else if (instruction == 'up') {
-          depth -= number;
+          aim -= number;
         } else if (instruction == 'down') {
-          depth += number;
+          aim += number;
         }
       }
     }
